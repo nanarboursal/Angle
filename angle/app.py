0620c4d434415dd28b3382b414b33434ee9a8744
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-09-01 19:52:42 -0700

    fixed backend

--- angle/app.py
+++ angle/app.py
@@ -3,17 +3,13 @@ from flask_cors import CORS, cross_origin
 import json
 import os
 
-app = Flask(__name__, static_folder='../client-app/build', static_url_path='/')
+app = Flask(__name__, static_folder='../angle-ui/build', static_url_path='/')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
-
-@app.route("/api/audio", methods=["POST","GET"])
-def Audio_Transcriber():
-    return "Hello World!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
