e6a237077b41c701bb2d07582c31e260bd76e86b
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-10-15 17:43:03 -0700

    Basic MongoDB connection

--- angle/app.py
+++ angle/app.py
@@ -2,10 +2,23 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 import json
 import os
+import db
+
+from flask_jwt_extended import JWTManager 
+from flask_jwt_extended import create_access_token
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId 
+from flask_bcrypt import Bcrypt 
+
+
 
 app = Flask(__name__, static_folder='../angle-ui/build', static_url_path='/')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+mongo = PyMongo(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
 
 @app.route('/')
 def index():
