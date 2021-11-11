95153ecaf7db58183cadcc325600b00fb14a5073
Edgar Villanueva <garvisteam@gmail.com>
2021-11-10 18:39:08 -0800

    created login ui

--- angle-ui/src/Pages/Login/Login.styles.js
+++ angle-ui/src/Pages/Login/Login.styles.js
@@ -5,6 +5,12 @@ export const AuthButton = styled(Button)`
     background: #fccb90;
     background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
     background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+    border-radius: 20px;
+	border: 1px solid #FF4B2B;
+    font-size: 12px;
+	font-weight: bold;
+	padding: 12px 45px;
+	letter-spacing: 1px;
 `;
 
 export const Center = styled.div`
@@ -13,5 +19,5 @@ export const Center = styled.div`
 `;
 
 export const Logo = styled.img`
-    width: 185px;
+    width: 225px;
 `;
