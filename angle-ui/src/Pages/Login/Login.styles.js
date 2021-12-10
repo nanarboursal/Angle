64b999381d1a0a490f8b00acba6f8b380e780aaf
Nanar Boursalian <nanar.boursalian@sjsu.edu>
2021-12-10 12:09:37 -0800

    login and signup page ui fixed

--- angle-ui/src/Pages/Login/Login.styles.js
+++ angle-ui/src/Pages/Login/Login.styles.js
@@ -2,9 +2,7 @@ import styled from 'styled-components';
 import { Button } from 'reactstrap';
 
 export const AuthButton = styled(Button)`
-    background: #fccb90;
-    background: -webkit-linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-    background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
+    background: #8a584c;
     border-radius: 20px;
 	border: 1px solid #FF4B2B;
     font-size: 12px;
